6d15a69dd7364c9b15b58007dbbcaaae1d022fb2
Alexander Kanev <alex99q@abv.bg>
2017-02-12 01:11:16 +0200

    Add files via upload

---
+++ SumSeconds/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var susteztel1 = int.Parse(Console.ReadLine());
+            var susteztel2 = int.Parse(Console.ReadLine());
+            var susteztel3 = int.Parse(Console.ReadLine());
+            int sustezteli = susteztel1 + susteztel2 + susteztel3;
+
+            if (sustezteli > 0 && sustezteli < 60)
+            {
+                if (sustezteli < 10)
+                {
+                    Console.WriteLine("0:0" + sustezteli);
+                }
+                else
+                {
+                    Console.WriteLine("0:" + sustezteli);
+                }
+            }
+
+            if (sustezteli > 59 && sustezteli < 120)
+            {
+                if (sustezteli - 60 < 10)
+                {
+                    Console.WriteLine("1:0" + (sustezteli - 60));
+                }
+                else
+                {
+                    Console.WriteLine("1:" + (sustezteli - 60));
+                }
+            }
+
+            if (sustezteli > 119 && sustezteli < 180)
+            {
+                if (sustezteli - 120 < 10)
+                {
+                    Console.WriteLine("2:0" + (sustezteli - 120));
+                }
+                else
+                {
+                    Console.WriteLine("2:" + (sustezteli - 120));
+                }
+            }
+
+        }
+    }
+}
---
+++ time+15min/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace time_15min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hour = int.Parse(Console.ReadLine());
+            var min = int.Parse(Console.ReadLine());
+
+            if (hour >= 0 && hour < 24 && min >= 0 && min < 60)
+            {
+                min = min + 15;
+                if(min > 59)
+                {
+                    hour++;
+                    min = min - 60;
+                }
+                if(hour == 24)
+                {
+                    hour = 0;
+                }
+                if(min < 10)
+                {
+                    Console.WriteLine(hour + ":0" + min);
+                }
+                else
+                {
+                    Console.WriteLine(hour + ":" + min);
+                }
+            }
+        }
+    }
+}
